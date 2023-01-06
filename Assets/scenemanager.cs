21de20d5d1056e709e8ec3e994190e4cbb926bb1
sapienhomo <105915520+Huuuuuge@users.noreply.github.com>
2023-01-06 11:50:15 +0530

    initial commit

---
+++ Assets/scenemanager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenemanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void oneplus()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); }
+
+    public void oneminus()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); }
+
+    public void twoplus()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); }
+
+    public void twominus()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2); }
+
+    public void threeplus()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3); }
+
+    public void threeminus()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3); }
+
+    public void fourplus()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4); }
+
+    public void fourminus()
+    { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4); }
+}
