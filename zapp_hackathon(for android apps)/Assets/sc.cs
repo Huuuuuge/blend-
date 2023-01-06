fdc13f39fd4093357df226e84ea911528475fa20
sapienhomo <105915520+Huuuuuge@users.noreply.github.com>
2023-01-06 16:06:32 +0530

    final

---
+++ zapp_hackathon(for android apps)/Assets/sc.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc : MonoBehaviour
+{
+    public void watch()
+    { Application.OpenURL("https://webxr.run/xvP08lk5x7pZL");
+        Debug.Log("is this work");
+    }
+
+    public void shirt()
+    {
+        Application.OpenURL("https://webxr.run/mPnDD54PgGVDE");
+        Debug.Log("is this work");
+    }
+
+    public void cap()
+    {
+        Application.OpenURL("https://webxr.run/Vp6JOb749e16x");
+        Debug.Log("is this work");
+    }
+
+    public void shoes()
+    {
+        Application.OpenURL("https://webxr.run/b5N07AmWAd0g");
+        Debug.Log("is this work");
+    }
+
+    public void shiba()
+    {
+        Application.OpenURL("https://webxr.run/5X6ypvONM92eM");
+        Debug.Log("is this work");
+    }
+
+    public void painting()
+    {
+        Application.OpenURL("https://webxr.run/V1N4mNPagYdMM");
+        Debug.Log("is this work");
+    }
+}
